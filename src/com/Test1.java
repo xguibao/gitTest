d45d999bf039d38b2ad4b12f7ab228844bccbf93
xugb <865719772@qq.com>
2018-10-15 16:03:32 +0800

    提交Test123

--- src/com/Test1.java
+++ src/com/Test1.java
@@ -2,6 +2,6 @@ package com;
 
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println("Test");System.out.println("Test");
     }
 }
